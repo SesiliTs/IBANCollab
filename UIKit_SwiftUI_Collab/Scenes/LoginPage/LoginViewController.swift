1fc15dfc44924607b2249dd930fc671f4a075721
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2024-01-11 15:36:21 +0400

    added firebase and network manager dependencies

---
+++ UIKit_SwiftUI_Collab/Scenes/LoginPage/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIKit_SwiftUI_Collab
+//
+//  Created by Sesili Tsikaridze on 11.01.24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    
+    
+
+
+}
+
---
+++ UIKit_SwiftUI_Collab/Services/AuthenticationManager.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationManager.swift
+//  UIKit_SwiftUI_Collab
+//
+//  Created by Sesili Tsikaridze on 11.01.24.
+//
+
+import Foundation
+import FirebaseAuth
+
+final class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    private init () { }
+    
+}
