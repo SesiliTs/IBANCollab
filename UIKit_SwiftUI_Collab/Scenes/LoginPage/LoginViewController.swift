0976c7f60cff4701cd6fbd9da523e99713a80aa6
Andria <andria.inas@gmail.com>
2024-01-14 15:46:43 +0400

    add main functionality

--- UIKit_SwiftUI_Collab/Scenes/LoginPage/LoginViewController.swift
+++ UIKit_SwiftUI_Collab/Scenes/LoginPage/LoginViewController.swift
@@ -15,10 +15,19 @@ class LoginViewController: UIViewController {
     private let emailTextField = CustomUITextField(placeholder: "Email")
     private let passwordTextField = CustomUITextField(placeholder: "Password")
     private let loginButton = CustomUIButton(title: "Login", hasBackground: true, fontSize: .medium)
-//    @IBOutlet private weak var emailTextField: CustomUITextField!
-//    @IBOutlet private weak var passwordTextField: CustomUITextField!
-//    @IBOutlet private weak var loginButton: CustomUIButton!
- 
+    private let signUpNowButton = {
+        let button = CustomUIButton(title: "SignUpNow", hasBackground: true, fontSize: .medium)
+        button.backgroundColor = .gray
+        return button
+    }()
+    private let mainStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 10
+        return stackView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +38,19 @@ class LoginViewController: UIViewController {
     private func setupUI() {
         
         loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        signUpNowButton.addTarget(self, action: #selector(signUpNowButtonTapped), for: .touchUpInside)
+
+        view.backgroundColor = .white
+        
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(emailTextField)
+        mainStackView.addArrangedSubview(passwordTextField)
+        mainStackView.addArrangedSubview(loginButton)
+        mainStackView.addArrangedSubview(signUpNowButton)
+        
+        mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
     }
  
     // MARK: - Actions
@@ -41,14 +63,19 @@ class LoginViewController: UIViewController {
         
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] (result, error) in
             if let error = error {
-                                print("Authentication failed: \(error.localizedDescription)")
+                self?.displayAlert(title: "Log In Error", message: "\(error.localizedDescription)")
                 
             } else {
-                print("ragaca")
-//                self?.navigateToNextScreen()
+                print("success")
+                //                self?.navigateToNextScreen()
             }
         }
     }
+    
+    @objc private func signUpNowButtonTapped() {
+        print("Navigate to sign Up Page")
+    }
+
  
     private func navigateToNextScreen() {
         let storyboard = UIStoryboard(name: "YourStoryboardName", bundle: nil)
@@ -57,4 +84,11 @@ class LoginViewController: UIViewController {
             navigationController?.pushViewController(nextViewController, animated: true)
         }
     }
+    
+    private func displayAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+
 }
