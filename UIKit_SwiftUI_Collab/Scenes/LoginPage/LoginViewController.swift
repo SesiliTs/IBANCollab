dada336cf617b42be19882b49fc948207cab1ac6
Andria <andria.inas@gmail.com>
2024-01-14 14:48:44 +0400

    first commit

--- UIKit_SwiftUI_Collab/Scenes/LoginPage/LoginViewController.swift
+++ UIKit_SwiftUI_Collab/Scenes/LoginPage/LoginViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
-
+import Firebase
+ 
 class LoginViewController: UIViewController {
-
+    
+ 
+    // MARK: - IBOutlets
+    private let emailTextField = CustomUITextField(placeholder: "Email")
+    private let passwordTextField = CustomUITextField(placeholder: "Password")
+    private let loginButton = CustomUIButton(title: "Login", hasBackground: true, fontSize: .medium)
+//    @IBOutlet private weak var emailTextField: CustomUITextField!
+//    @IBOutlet private weak var passwordTextField: CustomUITextField!
+//    @IBOutlet private weak var loginButton: CustomUIButton!
+ 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
+        setupUI()
     }
-    
-    
-
-
+ 
+    // MARK: - UI Setup
+    private func setupUI() {
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+ 
+    // MARK: - Actions
+    @objc private func loginButtonTapped() {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+            
+            return
+        }
+ 
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (result, error) in
+            if let error = error {
+                                print("Authentication failed: \(error.localizedDescription)")
+                
+            } else {
+                print("ragaca")
+//                self?.navigateToNextScreen()
+            }
+        }
+    }
+ 
+    private func navigateToNextScreen() {
+        let storyboard = UIStoryboard(name: "YourStoryboardName", bundle: nil)
+        if let nextViewController = storyboard.instantiateViewController(withIdentifier: "YourViewControllerIdentifier") as? LoginViewController {
+           
+            navigationController?.pushViewController(nextViewController, animated: true)
+        }
+    }
 }
-
